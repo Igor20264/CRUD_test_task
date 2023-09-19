32ccad0a2e0ca195c48727952517557bb0c0c96f
Igor20264 <76581675+Igor20264@users.noreply.github.com>
2023-09-20 00:44:52 +0300

    v 0.1 Beta Всё в одном файле далее разнести по разными файлам Работает регистрация пользователей

---
+++ main.py
@@ -0,0 +1,218 @@
+# 19.09.2023 21:39:45
+# Я Хлеб или же Игорь
+# Хз зачем ты читаешь это... Ты думаешь тут будет важная информация... Возможно...
+# Это стартовый файл, обычно тут ничего нет... Ищи в других файлах!
+
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+import json
+app = FastAPI()
+
+# Создаем подключение к базе данных (файл my_database.db будет создан)
+connection = sqlite3.connect('database.db', check_same_thread=False)
+@app.on_event("shutdown")
+async def shutdown():
+    connection.close()
+
+
+# Модель данных пользователя
+class User(BaseModel):
+    name: str
+    password: str
+
+# Хранилище данных для пользователей (пример - список пользователей)
+users = []
+
+
+# Модель данных бронирования
+class Booking(BaseModel):
+    start: datetime
+    duration: int
+    comment: Optional[str] = None
+
+
+# Хранилище данных для бронирования (пример - список бронирований)
+bookings = []
+
+def executes(text):
+    cursor = connection.cursor()
+    cursor.execute(text)
+    connection.commit()
+    return True
+
+def fetchal(text):
+    cursor = connection.cursor()
+    cursor.execute(text)
+    results = cursor.fetchall()
+    return results
+# Операция создания пользователя (Create)
+@app.post("/user/add", response_model=int)
+def create_user(user: User)->int:
+    executes(f"""
+    INSERT INTO User (username, password, created, updated)
+    VALUES ("{user.name}", "{user.password}", 0, 1);
+    """)
+    return fetchal(f"""
+    SELECT *
+    FROM User
+    ORDER BY id DESC
+    LIMIT 1;
+    """)[0][0]  # Возвращаем индекс созданного пользователя
+
+@app.post("/user/chekname", response_model=int)
+def create_user(user: User)->int:
+    executes(f"""
+    INSERT INTO User (username, password, created, updated)
+    VALUES ("{user.name}", "{user.password}", 0, 1);
+    """)
+    return fetchal(f"""
+    SELECT *
+    FROM User
+    ORDER BY id DESC
+    LIMIT 1;
+    """)[0][0]
+
+
+# Операция получения имени и id пользователя (Read)
+@app.get("/user/get_id_name", response_model=int)
+def get_user_id_by_name(name: str):
+    id = fetchal(f"""
+        SELECT *
+        FROM User
+        WHILE username LIKE '{name}'
+        LIMIT 1;
+        """)[0][0]
+    for i, user in enumerate(users):
+        if user.name == name:
+            return i
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+# Операция получения всех пользователей (Read All)
+@app.get("/user/getall", response_model=List[User])
+def get_all_users():
+    return users
+
+
+# Операция удаления пользователя (Delete)
+@app.delete("/user/{user_id}/del", response_model=bool)
+def delete_user(user_id: int, password: str):
+    print(user_id,password)
+    if user_id < 0 or user_id >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user = users[user_id]
+    if user.password == password:
+        users.pop(user_id)
+        return True
+    else:
+        return False
+
+
+# Операция получения времени регистрации пользователя (Read)
+@app.get("/user/{user_id}/get_reg_time", response_model=int)
+def get_user_registration_time(user_id: int, password: str):
+    if user_id < 0 or user_id >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user = users[user_id]
+    if user.password == password:
+        return user.registration_time  # Предположим, что вы храните время регистрации пользователя
+    else:
+        return False
+
+
+# Операция сброса пароля пользователя (Update)
+@app.put("/user/{user_id}/reset_password", response_model=bool)
+def reset_user_password(user_id: int, time_create: datetime):
+    if user_id < 0 or user_id >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user = users[user_id]
+    # Проверяем, что время создания пользователя не превышает указанное время
+    if (datetime.now() - user.registration_time).total_seconds() <= time_create.total_seconds():
+        # Выполняем сброс пароля
+        user.password = "new_password"
+        return True
+    else:
+        return False
+
+
+# Операции для ресурса бронирования аналогичными способами
+
+# Модель данных бронирования пользователя
+class UserBooking(BaseModel):
+    user_id: int
+    start: datetime
+    duration: int
+    comment: Optional[str] = None
+
+
+# Хранилище данных для бронирования пользователя
+user_bookings = []
+
+
+# Операция добавления бронирования для пользователя
+@app.post("/booking/{user}/add", response_model=int)
+def create_user_booking(user: int, booking: Booking):
+    if user < 0 or user >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_booking = UserBooking(user_id=user, **booking.dict())
+    user_bookings.append(user_booking)
+    return len(user_bookings) - 1
+
+
+# Операция получения всех бронирований для пользователя
+@app.get("/booking/{user}/getall", response_model=List[UserBooking])
+def get_all_user_bookings(user: int):
+    if user < 0 or user >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_bookings_list = [ub for ub in user_bookings if ub.user_id == user]
+    return user_bookings_list
+
+
+# Операция удаления бронирования для пользователя
+@app.delete("/booking/{user}/{booking_id}/del", response_model=bool)
+def delete_user_booking(user: int, booking_id: int, password: str):
+    if user < 0 or user >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if booking_id < 0 or booking_id >= len(user_bookings):
+        raise HTTPException(status_code=404, detail="Booking not found")
+
+    user_booking = user_bookings[booking_id]
+    if user_booking.user_id == user:
+        user_bookings.pop(booking_id)
+        return True
+    else:
+        return False
+
+
+# Операция обновления бронирования для пользователя
+@app.put("/booking/{user}/{booking_id}/update", response_model=bool)
+def update_user_booking(user: int, booking_id: int, booking: Booking, password: str):
+    if user < 0 or user >= len(users):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if booking_id < 0 or booking_id >= len(user_bookings):
+        raise HTTPException(status_code=404, detail="Booking not found")
+
+    user_booking = user_bookings[booking_id]
+    if user_booking.user_id == user:
+        user_booking.start = booking.start
+        user_booking.duration = booking.duration
+        user_booking.comment = booking.comment
+        return True
+    else:
+        return False
+
