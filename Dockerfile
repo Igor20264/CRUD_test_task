7c369208090f868a2e7f38e8007a05844efca185
Igor <tarasovigor2005@gmail.com>
2023-09-24 01:50:28 +0300

    V 3.2 Beta

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL authors="Hlebushek"
 EXPOSE 8000
 
 COPY ./requirements.txt /code/requirements.txt
-
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir --root-user-action=ignore --upgrade -r /code/requirements.txt
 COPY . .
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- test.py
+++ test.py
@@ -4,56 +4,6 @@ import requests
 import json
 import threading
 
-import unittest
-
-
-class SimpleTestCase(unittest.TestCase):
-    def setUp(self):
-        self.name = str(random.randint(1,1000))
-        self.password = str(random.randint(1000000,999999999))
-        self.data = 0
-
-    def tearDown(self) -> None:
-        pass
-
-    def testA(self):
-        s = requests.post("http://localhost:8000/user/add",json.dumps({"name":self.name,"password":self.password}))
-        self.data = id = s.json()["user_id"]
-        assert type(id) == int
-    
-    def testB(self):
-        s = requests.post(f"http://localhost:8000/user/checkname?username={str(random.randint(1,1000))}")
-        self.id = s.json()
-        assert type(self.id)== bool
-
-    def testC(self):
-        s = requests.post(f"http://localhost:8000/user/getid?username={self.name}")
-        id = s.json()
-        print(id,type(id))
-        assert type(id)
-    
-    def testD(self):
-        s = requests.get("http://localhost:8000/user/getall")
-        id = s.json()
-        assert type(id) == list
-
-    def testE(self):
-        s = requests.get(f"http://localhost:8000/user/{self.data}/get_reg_time?password={self.password}")
-        id = s.json()
-        print(id)
-        assert type(id)
-
-    def testE(self):
-        s = requests.put(f"http://localhost:8000/user/{id}/reset_password?created={time}&password={self.password}")
-        id = s.json()
-        print(id)
-        assert type(id)
-
-    
-    
-
-
-
 #s = requests.post("http://localhost:8000/user/add",json.dumps({"name":str(random.randint(1,1000)),"password":"1009"}))
 #id = s.json()
 #print(id,type(id))
@@ -84,6 +34,3 @@ print(s.text)
 
 s = requests.get(f"http://localhost:8000/booking/{id}/getall")
 print(s.text)'''
-
-if __name__ == "__main__":
-    unittest.main() # run all tests
