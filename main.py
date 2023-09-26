204ad191c1ce24eb8bbbb69bf1a2c01876383633
Igor <tarasovigor2005@gmail.com>
2023-09-26 07:06:13 +0300

    V 1.2.2

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ from src.user.router import router as router_user
 from src.booking.router import router as router_booking
 
 app = FastAPI()
-print("DSSSD")
+
 app.include_router(router_user)
 app.include_router(router_booking)
